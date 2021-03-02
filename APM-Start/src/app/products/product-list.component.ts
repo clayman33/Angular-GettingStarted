42068b26e877e6debf8664709af37e278263cfd8
clayman33 <33763757+clayman33@users.noreply.github.com>
2021-03-02 00:05:02 -0600

    master

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,11 @@
+import { from } from "rxjs";
+import {Component} from '@angular/Core';
+import { templateJitUrl } from "@angular/compiler";
+
+@Component ({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+
+}
